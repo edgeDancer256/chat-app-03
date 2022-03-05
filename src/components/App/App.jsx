c9fa7afa6f271ef555ba25c5579f7814ab53fdb9
edgeDancer256 <luciferstrider@gmail.com>
2022-03-05 21:31:00 +0530

    sign-up-form

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,10 +3,12 @@ import { Route, Switch } from 'react-router-dom';
 
 export const App = () => {
   return (
-    <Switch>
-      <Chat exact path="/" component={Chat} />
-      <Route path="/login" component={Login} />
-      <Route path="/signup" component={Signup} />
-    </Switch>
+    <div className="app">
+      <Switch>
+        <Chat exact path="/" component={Chat} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+      </Switch>
+    </div>
   );
 };
--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,3 +1,46 @@
+import { FormField } from 'components';
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Formik, Form } from 'formik';
+import { validationSchema, defaultValues } from './formikConfig';
+
 export const Signup = () => {
-  return <>In Signup</>;
+  const history = useHistory();
+  const [serverError, setServerError] = useState('');
+
+  return (
+    <div className="auth-form">
+      <h1>Sign Up</h1>
+      <Formik
+        onSubmit={() => console.log('Submitting')}
+        validateOnMount={true}
+        initialValues={defaultValues}
+        validationSchema={validationSchema}
+      >
+        {({ isValid, isSubmitting }) => (
+          <Form>
+            <FormField name="userName" label="User Name" />
+            <FormField name="email" label="Email" type="email" />
+            <FormField name="password" label="Password" type="password" />
+            <FormField
+              name="verifyPassword"
+              label="Verify Password"
+              type="password"
+            />
+
+            <div className="auth-link-container">
+              Already have an account?{' '}
+              <span className="auth-link" onClick={() => history.push('login')}>
+                Log In!
+              </span>
+            </div>
+
+            <button disabled={isSubmitting || !isValid} type="submit">
+              Sign Up
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 };
