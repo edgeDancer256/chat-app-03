cb24c7b3f8aabd9862b6e3fbf44afa88c4d6202b
edgeDancer256 <luciferstrider@gmail.com>
2022-03-05 20:04:32 +0530

    router and env vars

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import { Login, Signup, Chat } from 'components';
+import { Route, Switch } from 'react-router-dom';
+
+export const App = () => {
+  return (
+    <Switch>
+      <Chat exact path="/" component={Chat} />
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={Signup} />
+    </Switch>
+  );
+};
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,3 @@
+export const Login = () => {
+  return <>In Login</>;
+};
